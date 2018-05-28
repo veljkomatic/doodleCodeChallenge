f42a8cf5aac0f8a343b2967b575a6700194eb442
veljko <veljko@heybrolly.com>
2018-05-28 14:00:25 +0200

    add reducer for messages

--- src/Redux/reducers/messages.js
+++ src/Redux/reducers/messages.js
@@ -1,10 +1,55 @@
 import { actionTypes } from '../actionCreators/types';
 
 const INITIAL_STATE = {
+	messages: [],
+	loading: false,
+	error: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case actionTypes.FETCHING_MESSAGES:
+		return {
+			...state,
+			loading: true,
+			error: ''
+		};
+		case actionTypes.FETCHING_MESSAGES_SUCCESS: {
+			return {
+				...state,
+				loading: false,
+				error: '',
+				messages: action.payload 
+			};
+		}
+		case actionTypes.FETCHING_POLICIES_DATA_FAIL:
+			return {
+				...state,
+				loading: false,
+				error: action.payload
+			};
+		case actionTypes.CREATE_MESSAGE:
+			return {
+				...state,
+				loading: true,
+				error: ''
+			};
+		case actionTypes.CREATE_MESSAGES_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				error: '',
+				messages: {
+					...state.messages,
+					...action.payload
+				}
+			};
+		case actionTypes.CREATE_MESSAGES_FAIL:
+			return {
+				...state,
+				loading: false,
+				error: action.payload
+			};
 		default:
 			return state;
 	}
